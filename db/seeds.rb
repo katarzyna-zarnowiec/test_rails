922be29d832e5ceead1b6baa12d753c652dab940
Kasia <katarzyna.zarnowiec@gmail.com>
2016-02-03 15:22:13 +0100

    add example data to seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+4.times do |i|
+	new_image = "circulo#{i+1}_investment.png"
+	new_text = Faker::Lorem.sentences(4).join(' ')
+	Secret.create(image: new_image,
+							 	text: new_text)
+end
